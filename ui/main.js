a8bccde50fa9917083cb31827082e4099d9b12bc
Anandayodya <36561360+Anandayodya@users.noreply.github.com>
2018-02-23 20:12:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,11 @@
 console.log('Loaded!');
 alert("Welcome");
-var img=document.getElementById('madi');
+var marginleft=0;
+function moveright()
+{
+marginleft=marginleft+10;
+img.style.marginLeft=marginleft+'px';
+}
 img.onclick = function(){
-  img.style.marginLeft='180px';  
+var interval =setInterval(moveright,100); 
 };
